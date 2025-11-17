be356a5602228ab0890a6305e1c771d5280425fa
blakehartin <bh@bh>
2025-11-17 15:40:45 -0800

    enable fee

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 
 	"github.com/quantumcoinproject/quantum-coin-go/common"
@@ -21,6 +22,10 @@ func printHelp() {
 	fmt.Println(" Usage")
 	fmt.Println("--------")
 	fmt.Println("quantumswap-cli deploycore FROM_ADDRESS")
+	fmt.Println("      Set the following environment variables:")
+	fmt.Println("           DP_RAW_URL, DP_KEY_FILE_DIR or DP_KEY_FILE")
+	fmt.Println("quantumswap-cli enablefee FROM_ADDRESS CONTRACT_ADDRESS FEE TICK_SPACING")
+	fmt.Println("      Use 100 for FEE and 1 for TICK_SPACING")
 	fmt.Println("      Set the following environment variables:")
 	fmt.Println("           DP_RAW_URL, DP_KEY_FILE_DIR or DP_KEY_FILE")
 }
@@ -45,15 +50,21 @@ func main() {
 		}
 	}
 
-	if os.Args[1] == "balance" {
+	if os.Args[1] == "deploycore" {
 		DeployCore()
+	} else if os.Args[1] == "enablefee" {
+		EnableFee()
 	} else {
 		printHelp()
 	}
-
 }
 
 func DeployCore() {
+	if len(os.Args) < 3 {
+		printHelp()
+		return
+	}
+
 	from := os.Args[2]
 	if common.IsHexAddress(from) == false {
 		fmt.Println("Invalid address", from)
@@ -71,6 +82,55 @@ func DeployCore() {
 	}
 
 	err = deployCore(from)
+	if err != nil {
+		fmt.Println("error", err)
+		return
+	}
+}
+
+func EnableFee() {
+	if len(os.Args) < 6 {
+		printHelp()
+		return
+	}
+
+	from := os.Args[2]
+	if common.IsHexAddress(from) == false {
+		fmt.Println("Invalid address", from)
+		return
+	}
+
+	coreContractAddress := os.Args[3]
+	if common.IsHexAddress(coreContractAddress) == false {
+		fmt.Println("Invalid contract address", from)
+		return
+	}
+
+	feeArg := os.Args[4]
+	fee, err := strconv.ParseUint(feeArg, 10, 64)
+	if err != nil {
+		fmt.Println("Error parsing fee", err)
+		return
+	}
+
+	tickArg := os.Args[5]
+	tick, err := strconv.ParseUint(tickArg, 10, 64)
+	if err != nil {
+		fmt.Println("Error parsing tick", err)
+		return
+	}
+
+	ethConfirm, err := prompt.Stdin.PromptConfirm(fmt.Sprintf("Do you want to enableFee contract from %s, contract %s, fee %v, tick %v?", from, coreContractAddress, fee, tick))
+	if err != nil {
+		fmt.Println("error", err)
+		return
+	}
+	if ethConfirm != true {
+		fmt.Println("confirmation not made")
+		return
+	}
+
+	err = enableFee(from, coreContractAddress, int64(fee), int64(tick))
 	if err != nil {
 		fmt.Println("error", err)
 		return
--- util.go
+++ util.go
@@ -4,14 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/quantumcoinproject/quantum-coin-go/accounts/abi/bind"
-	"github.com/quantumcoinproject/quantum-coin-go/accounts/keystore"
-	"github.com/quantumcoinproject/quantum-coin-go/common"
-	"github.com/quantumcoinproject/quantum-coin-go/console/prompt"
-	"github.com/quantumcoinproject/quantum-coin-go/core/types"
-	"github.com/quantumcoinproject/quantum-coin-go/crypto/cryptobase"
-	"github.com/quantumcoinproject/quantum-coin-go/crypto/signaturealgorithm"
-	"github.com/quantumcoinproject/quantum-coin-go/ethclient"
 	"io/ioutil"
 	"log"
 	"math/big"
@@ -21,6 +13,15 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/quantumcoinproject/quantum-coin-go/accounts/abi/bind"
+	"github.com/quantumcoinproject/quantum-coin-go/accounts/keystore"
+	"github.com/quantumcoinproject/quantum-coin-go/common"
+	"github.com/quantumcoinproject/quantum-coin-go/console/prompt"
+	"github.com/quantumcoinproject/quantum-coin-go/core/types"
+	"github.com/quantumcoinproject/quantum-coin-go/crypto/cryptobase"
+	"github.com/quantumcoinproject/quantum-coin-go/crypto/signaturealgorithm"
+	"github.com/quantumcoinproject/quantum-coin-go/ethclient"
 )
 
 const GAS_LIMIT_ENV = "GAS_LIMIT"
@@ -47,7 +48,7 @@ func getGasLimit(defaultLimit uint64) (uint64, error) {
 	if len(gasLimitEnv) > 0 {
 		gasLimit, err := strconv.ParseUint(gasLimitEnv, 10, 64)
 		if err != nil {
-			fmt.Println("Error parsing gas limit, err")
+			fmt.Println("Error parsing gas limit", err)
 			return gasLimit, err
 		}
 		fmt.Println("Using gas limit passed using environment variable", gasLimit)
@@ -188,7 +189,7 @@ func deployCore(from string) error {
 
 	txnOpts.From = fromAddress
 	txnOpts.Nonce = big.NewInt(int64(nonce))
-	txnOpts.GasLimit, err = getGasLimit(uint64(1500000))
+	txnOpts.GasLimit, err = getGasLimit(uint64(6000000))
 	if err != nil {
 		return err
 	}
@@ -202,6 +203,69 @@ func deployCore(from string) error {
 	}
 
 	fmt.Println("Your request to deploy core contracts has been added to the queue for processing. Please check your account after 10 minutes.")
+	fmt.Println("The transaction hash for tracking this request is: ", tx.Hash(), "contractAddress", contractAddress)
+	fmt.Println()
+
+	time.Sleep(1000 * time.Millisecond)
+
+	return nil
+}
+
+func enableFee(from string, contractAddr string, fee int64, tick int64) error {
+	key, err := GetKey(from)
+	if err != nil {
+		return err
+	}
+
+	client, err := ethclient.Dial(rawURL)
+	if err != nil {
+		return err
+	}
+
+	fromAddress, err := cryptobase.SigAlg.PublicKeyToAddress(&key.PublicKey)
+
+	if err != nil {
+		return err
+	}
+
+	contractAddress := common.HexToAddress(contractAddr)
+
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return err
+	}
+
+	chainId, err := getChainId()
+	if err != nil {
+		return err
+	}
+	txnOpts, err := bind.NewKeyedTransactorWithChainID(key, big.NewInt(chainId))
+
+	if err != nil {
+		return err
+	}
+
+	txnOpts.From = fromAddress
+	txnOpts.Nonce = big.NewInt(int64(nonce))
+	txnOpts.GasLimit, err = getGasLimit(uint64(6000000))
+	if err != nil {
+		return err
+	}
+
+	txnOpts.Value = big.NewInt(0)
+
+	contract, err := core.NewCore(contractAddress, client)
+	if err != nil {
+		return err
+	}
+
+	var tx *types.Transaction
+	tx, err = contract.EnableFeeAmount(txnOpts, big.NewInt(fee), big.NewInt(tick))
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Your request to enable fee has been added to the queue for processing. Please check your account after 10 minutes.")
 	fmt.Println("The transaction hash for tracking this request is: ", tx.Hash(), "contractAddress", contractAddress)
 	fmt.Println()
 
